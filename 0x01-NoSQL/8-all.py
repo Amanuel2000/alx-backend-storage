5e222b7c158788a80293e21a3bc6acd936aa6dba
Amanuel2000 <amanuelf44@gmail.com>
2023-05-08 14:39:23 +0300

    No-SQL Solution for tasks

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -13,5 +13,5 @@ def list_all(mongo_collection):
     """Return list of all docs in collection"""
     if not mongo_collection:
         return []
-    docs = mongo_collection.find()
-    return [doc for doc in docs]
+    docmts = mongo_collection.find()
+    return [doc for doc in docmts]
